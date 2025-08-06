95f38c2efc54a597106c2cae00035bcdcb07553c
Drashti Pateliya <pateliyadrashti54@gmail.com>
2025-08-06 16:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (67.29%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public int[] plusOne(int[] digits) {
-        
+        //DD
         for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
