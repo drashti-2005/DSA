1bd03c11d128f314e561c5b9ed03e1c400df84fd
Drashti Pateliya <pateliyadrashti54@gmail.com>
2026-01-28 10:06:47 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (44.41%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -4,7 +4,7 @@ public:
         int left=0,right=nums.size()-1;
 
         while(left<=right){
-            int mid = left+(right-left)/2;
+            int mid = (left+right)/2;
             if(nums[mid]==target){
                 return mid;
             }else if(nums[mid]<target){ 
