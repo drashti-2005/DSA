7f0d2d911f34eff1ceb6d5c19e384087596a858f
Drashti Pateliya <pateliyadrashti54@gmail.com>
2026-02-01 09:26:49 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (20.68%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -4,11 +4,32 @@ public:
         int l=0, r=nums.size()-1;
 
         while(l<=r){
-            if(nums[l]!=target){
-                l++;
-            }else{
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target){
                 return true;
             }
+            while (l < mid && nums[l] == nums[mid])
+                l++;
+
+            while (mid < r && nums[r] == nums[mid])
+                r--;
+            if(nums[l]<=nums[mid]){
+                if(nums[l]<=target && nums[mid]>target){
+                    r=mid-1;
+                }else{
+                    l=mid+1;
+                }
+
+                
+            }else{
+                if(nums[mid]<target&&nums[r]>=target){
+                    l=mid+1;
+                }else{
+                    r=mid-1;
+                }
+            }
+
+            
         }
         return false;
     }
