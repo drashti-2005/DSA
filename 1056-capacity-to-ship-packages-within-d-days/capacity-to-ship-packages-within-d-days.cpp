07394aa3adc139df13e9d9f1b947de1ee06ca11e
Drashti Pateliya <pateliyadrashti54@gmail.com>
2026-02-15 08:31:28 +0530

    Time: 12 ms (51.41%) | Memory: 35 MB (86.47%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -16,7 +16,11 @@ public:
     int shipWithinDays(vector<int>& weights, int days) {
         int n=weights.size();
         int low=*max_element(weights.begin(),weights.end());
-        int high=accumulate(weights.begin(), weights.end(), 0);
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum+=weights[i];
+        }
+        int high=sum;
 
         while(low<=high){
             int mid=low+(high-low)/2;
