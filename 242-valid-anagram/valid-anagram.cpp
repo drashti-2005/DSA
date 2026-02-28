b44171866ed99eb68745701a74fb2177110e6891
Drashti Pateliya <pateliyadrashti54@gmail.com>
2026-02-28 15:50:24 +0530

    Time: 1 ms (55.46%) | Memory: 9.6 MB (91.30%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,12 +3,21 @@ public:
     bool isAnagram(string s, string t) {
         if(s.length()!=t.length())
         return false;
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t){
-            return true;
-        }
-        return false;
+        int count[26] = {0};
+
+    for(int i = 0; i < s.length(); i++)
+    {
+        count[s[i]-'a']++;
+        count[t[i]-'a']--;
+    }
+
+    for(int i = 0; i < 26; i++)
+    {
+        if(count[i] != 0)
+            return false;
+    }
+
+    return true;
 
     }
 };
