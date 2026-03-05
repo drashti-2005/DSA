5bc34ebb80273f6dcaf73b08a41787350e509f56
Drashti Pateliya <pateliyadrashti54@gmail.com>
2026-03-05 13:11:58 +0530

    Time: 111 ms (25.62%) | Memory: 26.2 MB (28.07%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-
+        if (s.empty()) return "";
         int n = s.size();
         vector<vector<bool>> dp(n, vector<bool>(n,false));
 
