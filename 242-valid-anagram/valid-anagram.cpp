88ab29a8a4a69fa7b1a3cd1641529979bf31d54c
Drashti Pateliya <pateliyadrashti54@gmail.com>
2026-07-11 11:29:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (92.61%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,8 +4,18 @@ public:
         if(s.size()!=t.size()){
             return false;
         }
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        int freq[26]={0};
+        for(char ch:s){
+            freq[ch-'a']++;
+        }
+        for(char ch:t){
+            freq[ch-'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(freq[i]!=0)
+                return false;
+            
+        }
+        return true;
     }
 };
