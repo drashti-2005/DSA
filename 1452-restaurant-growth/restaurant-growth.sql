1cff89ebcf13994b1927fe32073a19c0da1b7a45
Drashti Pateliya <pateliyadrashti54@gmail.com>
2026-08-03 14:16:35 +0530

    Time: 434 ms (16.54%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+select visited_on,amount,round(amount/7,2) as average_amount
+from (select visited_on,
+        sum(amount)over(order by visited_on rows between 6 preceding and current row) as amount,
+        row_number()over(order by visited_on) as rn
+        from (select visited_on,sum(amount) as amount
+                from Customer
+                group by visited_on) d
+    )t
+where rn>=7
+order by visited_on;
+
+
